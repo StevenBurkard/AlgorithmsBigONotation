26cdb11bcf5f3fe8114298f31a90354783af0b4d
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-06-02 11:49:42 -0500

    feat: added a function to check if a list containing numbers has all numbers less than 100

--- algorithms.py
+++ algorithms.py
@@ -1,9 +1,24 @@
-# Time complexity for Task 1 is Constant time- O(1)
-def is_even(n):
-    return n % 2 == 0
+# Time complexity for Task 1 is Constant time = O(1)
+#def is_even(n):
+#    return n % 2 == 0
 
-user_number = int(input("Enter a number: "))
+#user_number = int(input("Enter a number: "))
 
-result = is_even(user_number)
-print(f"{user_number} is {result}")
+#result = is_even(user_number)
+#print(f"{user_number} is {result}")
 
+# Time complexity for Task 2 is Linear Time = O(n)
+def check_numbers(n):
+    for number in n:
+        if number >= 100:
+            return False
+    return True
+
+def display_results():
+    print(f"First list is {check_numbers(first_list)}")
+    print(f"Second list is {check_numbers(second_list)}")
+
+first_list = [2, 24, 68, 77, 99, 98]
+second_list = [20, 100, 96, 97, 35]
+
+display_results()
