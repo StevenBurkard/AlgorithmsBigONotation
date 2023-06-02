b7de4bf0289bab1e11c7b4a7bec957af9c6b8da2
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-06-02 14:37:53 -0500

    added check_duplicat_names function to check a list for any duplicate names and a display_output function to display the results

--- algorithms.py
+++ algorithms.py
@@ -8,17 +8,33 @@
 #print(f"{user_number} is {result}")
 
 # Time complexity for Task 2 is Linear Time = O(n)
-def check_numbers(n):
-    for number in n:
-        if number >= 100:
-            return False
-    return True
+#def check_numbers(n):
+#    for number in n:
+#        if number >= 100:
+#            return False
+#    return True
 
-def display_results():
-    print(f"First list is {check_numbers(first_list)}")
-    print(f"Second list is {check_numbers(second_list)}")
+#def display_results():
+#    print(f"First list is {check_numbers(first_list)}")
+#    print(f"Second list is {check_numbers(second_list)}")
 
-first_list = [2, 24, 68, 77, 99, 98]
-second_list = [20, 100, 96, 97, 35]
+#first_list = [2, 24, 68, 77, 99, 98]
+#second_list = [20, 100, 96, 97, 35]
 
-display_results()
+#display_results()
+
+#Time complexity for Task 3 is Linear Time = O(n)
+def check_duplicate_names(names):
+    names_dictionary = {}
+    for name in names:
+        if name in names_dictionary:
+            return True
+        names_dictionary[name] = names_dictionary.get(name, 0) + 1
+    return False
+
+def display_outcome(names):
+    result = check_duplicate_names(names)
+    print(f"{names} is {result}!")
+
+display_outcome(["John", "Jacob", "Jason", "Bob", "Doug"])
+display_outcome(["Steven", "Terry", "Megan", "Zac", "Steven"])
